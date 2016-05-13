446cf1e53935333e75681fe7d06840d20c965d4d
jiayx <jiayanxiang@fangdd.com>
2016-05-13 20:40:48 +0800

    update

---
+++ bootstrap/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+// composer自动加载
+require __DIR__.'/../vendor/autoload.php';
+
+$app = new Raam\Application(realpath(__DIR__.'/../'));
+
---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+    exit('PHP版本必须高于5.4');
+}
+
+define('APP_DEBUG', true);
+define('APP_FOLDER', 'app');
+define('ENV', 'development');
+
+// 应用根目录
+define('ROOT_PATH', realpath(__DIR__ . '/../') . DIRECTORY_SEPARATOR);
+
+// 修正cli模式下运行目录不同部分系统函数取值不同的问题 比如: getcwd
+defined('STDIN') AND chdir(ROOT_PATH);
+
+require ROOT_PATH . 'bootstrap' . DIRECTORY_SEPARATOR .'bootstrap.php';
